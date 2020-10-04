f486d352fd4f05485c4edeebc0756fb3be67e01a
Victor Trajano <trajanodev@hotmail.com>
2020-10-04 16:01:36 -0400

    Front End com ReactJS consumindo API de produtos

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/header/'
+import './styles.css'
+import Routes from './routes'
+
+
+
+const app = () => (
+  <div className="App">
+    <Header/>
+    <Routes/>
+  </div>
+)
+
+
+
+export default app;
---
+++ src/pages/product/index.js
@@ -0,0 +1,54 @@
+import React , {Component} from 'react' 
+import api from '../../services/api'
+import './styles.css'
+
+
+export default class Product extends Component {
+   
+    state = {
+       product: {} ,
+       products : {}
+
+    }
+    
+    async componentDidMount() {
+       
+        const { id } =  this.props.match.params
+
+
+        const response = await api.get(`/products/${id}`)
+
+        this.setState({product: response.data})
+
+    }
+
+    
+
+   
+    
+    render ()   {
+
+
+        const { product }  = this.state
+
+        
+        return (
+            <div className = "product-info">
+                <h1>{product.title}</h1>
+                    <p>_id: {product._id}</p>
+                <p>{product.description}</p>
+
+                <p>
+                    URL: <a href={product.url} > {product.url}</a>
+                </p>
+
+                <button>Deletar</button>
+
+
+            </div>
+
+          
+        )
+                 
+    }
+}
